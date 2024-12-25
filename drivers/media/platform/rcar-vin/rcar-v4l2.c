renesas-rcar/linux-bsp

media: rcar-vin: Add callback function for cropcap
This patch supports cropcap in case of using media-ctl.
So register rvin_g_selection and rvin_s_selection in rvin_mc_ioctl_ops.

Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>
(cherry picked from commit af81f41094738e6676d222fdad62f80a1d590274)
Signed-off-by: Linh Phung <linh.phung.jy@renesas.com>

drivers/media/platform/renesas/rcar-vin/rcar-v4l2.c
@@ -318,6 +318,41 @@ done:
 	return ret;
 }
 
+static int rvin_get_sd_format(struct rvin_dev *vin, struct v4l2_pix_format *pix)
+{
+	struct v4l2_subdev *sd;
+	struct media_pad *pad;
+	struct v4l2_subdev_format fmt = {
+		.which = V4L2_SUBDEV_FORMAT_ACTIVE,
+	};
+
+	/* Get cropping size */
+	pad = media_pad_remote_pad_first(&vin->pad);
+	if (!pad)
+		return -EPIPE;
+
+	sd = media_entity_to_v4l2_subdev(pad->entity);
+	if (!sd)
+		return -EPIPE;
+
+	if (v4l2_subdev_call(sd, pad, get_fmt, NULL, &fmt))
+		return -EPIPE;
+
+	vin->src_rect.width = pix->width = fmt.format.width;
+	vin->src_rect.height = pix->height = fmt.format.height;
+	vin->crop = vin->src_rect;
+
+	if (fmt.format.field == V4L2_FIELD_ALTERNATE)
+		vin->format.field = V4L2_FIELD_INTERLACED;
+	else
+		vin->format.field = fmt.format.field;
+
+	vin->format.bytesperline =
+		rvin_format_bytesperline(vin, &vin->format);
+
+	return 0;
+}
+
 static int rvin_querycap(struct file *file, void *priv,
 			 struct v4l2_capability *cap)
 {
@@ -441,6 +476,7 @@ static int rvin_g_selection(struct file *file, void *fh,
 			    struct v4l2_selection *s)
 {
 	struct rvin_dev *vin = video_drvdata(file);
+	int ret;
 
 	if (s->type != V4L2_BUF_TYPE_VIDEO_CAPTURE)
 		return -EINVAL;
@@ -448,6 +484,11 @@ static int rvin_g_selection(struct file *file, void *fh,
 	switch (s->target) {
 	case V4L2_SEL_TGT_CROP_BOUNDS:
 	case V4L2_SEL_TGT_CROP_DEFAULT:
+		if (vin->info->use_mc) {
+			ret = rvin_get_sd_format(vin, &vin->format);
+			if (ret)
+				return ret;
+		}
 		s->r.left = s->r.top = 0;
 		s->r.width = vin->src_rect.width;
 		s->r.height = vin->src_rect.height;
@@ -869,6 +910,9 @@ static const struct v4l2_ioctl_ops rvin_mc_ioctl_ops = {
 	.vidioc_s_fmt_vid_cap		= rvin_mc_s_fmt_vid_cap,
 	.vidioc_enum_fmt_vid_cap	= rvin_enum_fmt_vid_cap,
 
+	.vidioc_g_selection		= rvin_g_selection,
+	.vidioc_s_selection		= rvin_s_selection,
+
 	.vidioc_reqbufs			= vb2_ioctl_reqbufs,
 	.vidioc_create_bufs		= vb2_ioctl_create_bufs,
 	.vidioc_querybuf		= vb2_ioctl_querybuf,
@@ -919,6 +963,8 @@ static int rvin_open(struct file *file)
 	ret = v4l2_fh_open(file);
 	if (ret)
 		goto err_unlock;
+
+	rvin_get_sd_format(vin, &vin->format);
 
 	if (vin->info->use_mc)
 		ret = v4l2_pipeline_pm_get(&vin->vdev.entity);

ThuanHa010/linux-bsp-src

drivers/media/platform/rcar-vin/rcar-v4l2.c
@@ -309,6 +309,41 @@ done:
 	return ret;
 }
 
+static int rvin_get_sd_format(struct rvin_dev *vin, struct v4l2_pix_format *pix)
+{
+	struct v4l2_subdev *sd;
+	struct media_pad *pad;
+	struct v4l2_subdev_format fmt = {
+		.which = V4L2_SUBDEV_FORMAT_ACTIVE,
+	};
+
+	/* Get cropping size */
+	pad = media_entity_remote_pad(&vin->pad);
+	if (!pad)
+		return -EPIPE;
+
+	sd = media_entity_to_v4l2_subdev(pad->entity);
+	if (!sd)
+		return -EPIPE;
+
+	if (v4l2_subdev_call(sd, pad, get_fmt, NULL, &fmt))
+		return -EPIPE;
+
+	vin->src_rect.width = pix->width = fmt.format.width;
+	vin->src_rect.height = pix->height = fmt.format.height;
+	vin->crop = vin->src_rect;
+
+	if (fmt.format.field == V4L2_FIELD_ALTERNATE)
+		vin->format.field = V4L2_FIELD_INTERLACED;
+	else
+		vin->format.field = fmt.format.field;
+
+	vin->format.bytesperline =
+		rvin_format_bytesperline(vin, &vin->format);
+
+	return 0;
+}
+
 static int rvin_querycap(struct file *file, void *priv,
 			 struct v4l2_capability *cap)
 {
@@ -436,6 +471,7 @@ static int rvin_g_selection(struct file *file, void *fh,
 			    struct v4l2_selection *s)
 {
 	struct rvin_dev *vin = video_drvdata(file);
+	int ret;
 
 	if (s->type != V4L2_BUF_TYPE_VIDEO_CAPTURE)
 		return -EINVAL;
@@ -443,6 +479,11 @@ static int rvin_g_selection(struct file *file, void *fh,
 	switch (s->target) {
 	case V4L2_SEL_TGT_CROP_BOUNDS:
 	case V4L2_SEL_TGT_CROP_DEFAULT:
+		if (vin->info->use_mc) {
+			ret = rvin_get_sd_format(vin, &vin->format);
+			if (ret)
+				return ret;
+		}
 		s->r.left = s->r.top = 0;
 		s->r.width = vin->src_rect.width;
 		s->r.height = vin->src_rect.height;
@@ -843,6 +884,9 @@ static const struct v4l2_ioctl_ops rvin_mc_ioctl_ops = {
 	.vidioc_s_fmt_vid_cap		= rvin_mc_s_fmt_vid_cap,
 	.vidioc_enum_fmt_vid_cap	= rvin_enum_fmt_vid_cap,
 
+	.vidioc_g_selection		= rvin_g_selection,
+	.vidioc_s_selection		= rvin_s_selection,
+
 	.vidioc_reqbufs			= vb2_ioctl_reqbufs,
 	.vidioc_create_bufs		= vb2_ioctl_create_bufs,
 	.vidioc_querybuf		= vb2_ioctl_querybuf,
@@ -895,6 +939,8 @@ static int rvin_open(struct file *file)
 	ret = v4l2_fh_open(file);
 	if (ret)
 		goto err_unlock;
+
+	rvin_get_sd_format(vin, &vin->format);
 
 	if (vin->info->use_mc)
 		ret = v4l2_pipeline_pm_get(&vin->vdev.entity);